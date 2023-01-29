ab42c6c0b6028f90482c8aa460defe4700eba6b0
nikhil vanamala <nickhil215@gmail.com>
2023-01-29 15:54:15 +0530

    2

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -40,7 +40,7 @@ public class WhatsappRepository {
             groupMap.put(group,users);
             return group;
         }else {
-            Group group=new Group("Group "+numberOfGroups,users.size());
+            Group group=new Group("Group "+ ++numberOfGroups,users.size());
             groupMap.put(group,users);
             return group;
         }
@@ -90,6 +90,84 @@ public class WhatsappRepository {
         }
 
         return groupMessagesList.get(group).size();
+    }
+
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        //Throw "Group does not exist" if the mentioned group does not exist
+        //Throw "Approver does not have rights" if the approver is not the current admin of the group
+        //Throw "User is not a participant" if the user is not a part of the group
+        //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
+
+        if(!groupMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+        if(!groupMap.get(0).equals(approver)){
+            throw new Exception("Approver does not have rights");
+        }
+          boolean isUser=false;
+        for(User user1:groupMap.get(group)){
+            if (user1.equals(user1)){
+                isUser=true;
+            }
+        }
+
+        if(!isUser){
+            throw new Exception("User is not a participant");
+        }
+         List<User> users=groupMap.get(group);
+        for( User user1: users){
+            if(user1.equals(user)){
+               Collections.swap(users,users.indexOf(user),users.indexOf(user1));
+            }
+        }
+
+       return "SUCCESS";
+    }
+
+    public int removeUser(User user) throws Exception {
+        //A user belongs to exactly one group
+        //If user is not found in any group, throw "User not found" exception
+        //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
+        //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
+        //If user is removed successfully, return (the updated number of users in the group + the updated number of messages in group + the updated number of overall messages)
+      boolean isGroupPerson=false;
+      Group group1=null;
+        for(Group group:groupMap.keySet()){
+            for(User user1:groupMap.get(group)){
+                if(user1.equals(user)){
+                    isGroupPerson=true;
+                    group1=group;
+                }
+            }
+        }
+        if(!isGroupPerson){
+            throw new Exception("User not found");
+        }
+
+        if(groupMap.get(group1).get(0).equals(user)){
+            throw new Exception("Cannot remove admin");
+        }
+//
+//        deleting msgs from list of groupMessages
+        for(Group group:groupMessagesList.keySet()){
+            for(Message message:groupMessagesList.get(group)){
+                if(userMessageList.get(user).contains(message)){
+                    groupMessagesList.remove(message);
+                }
+            }
+        }
+//        deleting msgs from list of created msgs;
+        for(Message message:messageList){
+            if(userMessageList.get(user).contains(message)){
+                messageList.remove(message);
+            }
+            }
+        userMessageList.remove(user);
+        groupMap.remove(user);
+//If user is removed successfully, return (the updated number of users in the group + the updated number of messages in group + the updated number of overall messages)
+
+            return groupMap.get(group1).size()+messageList.size()+groupMessagesList.get(group1).size();
     }
 
 
